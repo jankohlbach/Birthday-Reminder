a6655303392a6290981272cdcd0d1d5800a1808b
Jan Kohlbach <mail@jankohlbach.dev>
2019-09-09 22:12:56 +0200

    refactor code to class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,170 +1,200 @@
 (() => {
-  let birthdays = [];
-  const form = document.querySelector('form');
-  const dayInput = form.querySelector('#day');
-  const monthInput = form.querySelector('#month');
-  const yearInput = form.querySelector('#year');
-  const nameInput = form.querySelector('#name');
-  const infoInput = form.querySelector('#info');
-  const list = document.querySelector('.list');
+  class Birthday {
+    constructor() {
+      this.birthdays = [];
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const LOCAL_STORAGE_KEY = 'birthdays';
+      this.LOCAL_STORAGE_KEY = 'birthdays';
 
-  const setLocalStorage = (key, value) => localStorage.setItem(key, JSON.stringify(value));
-
-  const getLocalStorage = key => JSON.parse(localStorage.getItem(key));
-
-  const initYearInput = () => {
-    const currentYear = new Date().getFullYear();
-
-    for (let i = 0; i <= 100; i += 1) {
-      const option = document.createElement('option');
-      const yearToAdd = currentYear - i;
-      option.value = yearToAdd;
-      option.textContent = yearToAdd;
-      yearInput.appendChild(option);
+      this.validateDay = this.validateDay.bind(this);
+      this.validateMonth = this.validateMonth.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleButtonClick = this.handleButtonClick.bind(this);
+      this.initApp();
     }
-  };
 
-  const validateDay = () => {
-    const months31 = ['01', '03', '05', '07', '08', '10', '12'];
-    const option30 = dayInput.querySelector('option[value="30"]');
-    const option31 = dayInput.querySelector('option[value="31"]');
-
-    if (!months31.includes(monthInput.value)) {
-      option31.setAttribute('disabled', '');
-      if (dayInput.value === '31') dayInput.value = '';
-
-      if (monthInput.value === '2') {
-        option30.setAttribute('disabled', '');
-        if (dayInput.value === '30' || dayInput.value === '31') dayInput.value = '';
-      }
-    } else {
-      [option30, option31].forEach(option => option.removeAttribute('disabled'));
+    // helpers
+    static setLocalStorage(key, value) {
+      localStorage.setItem(key, JSON.stringify(value));
     }
-  };
 
-  const validateMonth = () => {
-    const months30 = ['04', '06', '09', '11'];
-    const option2 = monthInput.querySelector('option[value="02"]');
-
-    if (dayInput.value === '30') {
-      option2.setAttribute('disabled', '');
-      months30.forEach(month => monthInput.querySelector(`option[value="${month}"]`).removeAttribute('disabled'));
-    } else if (dayInput.value === '31') {
-      option2.setAttribute('disabled', '');
-      months30.forEach(month => monthInput.querySelector(`option[value="${month}"]`).setAttribute('disabled', ''));
-    } else {
-      option2.removeAttribute('disabled');
+    static getLocalStorage(key) {
+      return JSON.parse(localStorage.getItem(key));
     }
-  };
 
-  const generateHash = () => {
-    const currentDate = new Date().getTime();
-    const random = Math.floor(Math.random() * 100);
-    const hash = currentDate.toString() + random.toString();
-    return hash;
-  };
+    static generateHash() {
+      const currentDate = new Date().getTime();
+      const random = Math.floor(Math.random() * 100);
+      const hash = currentDate.toString() + random.toString();
+      return hash;
+    }
 
-  const sortList = (array) => {
-    array.sort((a, b) => {
-      if (a.month > b.month) {
-        return 1;
-      }
-
-      if (a.month === b.month) {
-        if (a.day > b.day) {
-          return 1;
+    validateDay() {
+      if (!this.MONTHS_31.includes(this.monthInput.value)) {
+        this.MONTH_OPTION_31.setAttribute('disabled', '');
+        if (this.monthInput.value === '02') {
+          this.MONTH_OPTION_30.setAttribute('disabled', '');
+        } else {
+          this.MONTH_OPTION_30.removeAttribute('disabled');
         }
+      } else {
+        [this.MONTH_OPTION_30, this.MONTH_OPTION_31].forEach(option => option.removeAttribute('disabled'));
       }
-
-      return null;
-    });
-  };
-
-  const updateList = () => {
-    birthdays = getLocalStorage(LOCAL_STORAGE_KEY);
-
-    const date = new Date();
-    const currentYear = date.getFullYear();
-
-    list.innerHTML = '';
-    let month;
-
-    birthdays.forEach((birthday) => {
-      if (month !== birthday.month) {
-        const monthElement = document.createElement('div');
-        monthElement.classList.add('month');
-        monthElement.innerHTML = `
-          <span class="month">${months[parseInt(birthday.month, 10) - 1]}</span>
-          <span class="year">${currentYear}</span>
-        `;
-        list.appendChild(monthElement);
-      }
-
-      const listElement = document.createElement('div');
-      listElement.classList.add('list-item');
-      listElement.id = birthday.hash;
-      listElement.innerHTML = `
-        <span class="date">${birthday.day}</span>
-        <span class="name">${birthday.name}</span>
-        <button class="edit">Edit</button>
-        <button class="delete">Delete</button>
-      `;
-      list.appendChild(listElement);
-
-      ({ month } = birthday);
-    });
-  };
-
-  const updateAll = () => {
-    setLocalStorage(LOCAL_STORAGE_KEY, birthdays);
-    updateList();
-  };
-
-  const getFormData = (e) => {
-    e.preventDefault();
-
-    return {
-      day: dayInput.value,
-      month: monthInput.value,
-      year: yearInput.value,
-      name: nameInput.value,
-      info: infoInput.value,
-      hash: generateHash(),
-    };
-  };
-
-  const handleSubmit = (e) => {
-    const newEntry = getFormData(e);
-    birthdays.push(newEntry);
-    sortList(birthdays);
-    updateAll();
-    form.reset();
-  };
-
-  const handleButtonClick = (e) => {
-    if (e.target.tagName.toLowerCase() !== 'button') return;
-
-    const button = e.target;
-
-    if (button.classList.contains('delete')) {
-      const idToDelete = button.parentNode.id;
-      const indexToDelete = birthdays.findIndex(birthday => birthday.hash === idToDelete);
-
-      birthdays.splice(indexToDelete, 1);
-
-      updateAll();
     }
-  };
 
-  initYearInput();
-  if (getLocalStorage(LOCAL_STORAGE_KEY)) {
-    updateList();
+    validateMonth() {
+      switch (this.dayInput.value) {
+        case '30':
+          this.MONTH_OPTION_2.setAttribute('disabled', '');
+          this.MONTHS_30_OPTIONS.forEach(option => option.removeAttribute('disabled'));
+          break;
+        case '31':
+          this.MONTH_OPTION_2.setAttribute('disabled', '');
+          this.MONTHS_30_OPTIONS.forEach(option => option.setAttribute('disabled', ''));
+          break;
+        default:
+          this.MONTH_OPTION_2.removeAttribute('disabled');
+          this.MONTHS_30_OPTIONS.forEach(option => option.removeAttribute('disabled'));
+          break;
+      }
+    }
+
+    static sortList(array) {
+      array.sort((a, b) => {
+        if (a.month > b.month) return 1;
+
+        if (a.month === b.month) {
+          if (a.day > b.day) return 1;
+        }
+
+        return null;
+      });
+    }
+
+    // initialization
+    initNodeElements() {
+      this.form = document.querySelector('form');
+      this.dayInput = this.form.querySelector('#day');
+      this.monthInput = this.form.querySelector('#month');
+      this.yearInput = this.form.querySelector('#year');
+      this.nameInput = this.form.querySelector('#name');
+      this.infoInput = this.form.querySelector('#info');
+      this.list = document.querySelector('.list');
+    }
+
+    initYearInput() {
+      const currentYear = new Date().getFullYear();
+
+      for (let i = 0; i <= 100; i += 1) {
+        const option = document.createElement('option');
+        const yearToAdd = currentYear - i;
+        option.value = yearToAdd;
+        option.textContent = yearToAdd;
+        this.yearInput.appendChild(option);
+      }
+    }
+
+    setUpMonthVariables() {
+      this.MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      this.MONTHS_30 = ['04', '06', '09', '11'];
+      this.MONTHS_31 = ['01', '03', '05', '07', '08', '10', '12'];
+      this.MONTHS_30_OPTIONS = this.MONTHS_30.map(month => this.monthInput.querySelector(`option[value="${month}"]`));
+      this.MONTH_OPTION_2 = this.monthInput.querySelector('option[value="02"]');
+      this.MONTH_OPTION_30 = this.dayInput.querySelector('option[value="30"]');
+      this.MONTH_OPTION_31 = this.dayInput.querySelector('option[value="31"]');
+    }
+
+    setUpEventListeners() {
+      this.dayInput.addEventListener('change', this.validateMonth);
+      this.monthInput.addEventListener('change', this.validateDay);
+      this.form.addEventListener('submit', this.handleSubmit);
+      this.list.addEventListener('click', this.handleButtonClick);
+    }
+
+    initApp() {
+      this.initNodeElements();
+      this.initYearInput();
+      this.setUpMonthVariables();
+      this.setUpEventListeners();
+      if (Birthday.getLocalStorage(this.LOCAL_STORAGE_KEY)) this.updateList();
+    }
+
+    // submit
+    getFormData(e) {
+      e.preventDefault();
+
+      return {
+        day: this.dayInput.value,
+        month: this.monthInput.value,
+        year: this.yearInput.value,
+        name: this.nameInput.value,
+        info: this.infoInput.value,
+        hash: Birthday.generateHash(),
+      };
+    }
+
+    updateList() {
+      this.birthdays = Birthday.getLocalStorage(this.LOCAL_STORAGE_KEY);
+
+      const date = new Date();
+      const currentYear = date.getFullYear();
+
+      this.list.innerHTML = '';
+      let month;
+
+      this.birthdays.forEach((birthday) => {
+        if (month !== birthday.month) {
+          const monthElement = document.createElement('div');
+          monthElement.classList.add('month');
+          monthElement.innerHTML = `
+            <span class="month">${this.MONTH_NAMES[parseInt(birthday.month, 10) - 1]}</span>
+            <span class="year">${currentYear}</span>
+          `;
+          this.list.appendChild(monthElement);
+        }
+
+        const listElement = document.createElement('div');
+        listElement.classList.add('list-item');
+        listElement.id = birthday.hash;
+        listElement.innerHTML = `
+          <span class="date">${birthday.day}</span>
+          <span class="name">${birthday.name}</span>
+          <button class="edit">Edit</button>
+          <button class="delete">Delete</button>
+        `;
+        this.list.appendChild(listElement);
+
+        ({ month } = birthday);
+      });
+    }
+
+    updateAll() {
+      Birthday.setLocalStorage(this.LOCAL_STORAGE_KEY, this.birthdays);
+      this.updateList();
+    }
+
+    handleSubmit(e) {
+      const newEntry = this.getFormData(e);
+      this.birthdays.push(newEntry);
+      Birthday.sortList(this.birthdays);
+      this.updateAll();
+      this.form.reset();
+    }
+
+    // button click
+    handleButtonClick(e) {
+      if (e.target.tagName.toLowerCase() !== 'button') return;
+
+      const button = e.target;
+
+      if (button.classList.contains('delete')) {
+        const idToDelete = button.parentNode.id;
+        const indexToDelete = this.birthdays.findIndex(birthday => birthday.hash === idToDelete);
+        this.birthdays.splice(indexToDelete, 1);
+        this.updateAll();
+      }
+    }
   }
-  dayInput.addEventListener('change', validateMonth);
-  monthInput.addEventListener('change', validateDay);
-  form.addEventListener('submit', handleSubmit);
-  list.addEventListener('click', handleButtonClick);
+
+  // eslint-disable-next-line no-unused-vars
+  const birthday = new Birthday();
 }).call(this);
