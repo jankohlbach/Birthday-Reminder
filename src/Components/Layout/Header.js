fe8a00a187b8c29cebb416d33ebd93fbcd7d0e84
Jan Kohlbach <mail@jankohlbach.dev>
2020-02-23 18:07:49 +0100

    add notification icon in header

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ContentWrap from './ContentWrap';
+import { subscribePush, unsubscribePush } from '../../helpers/subscription';
 
 export default function Header() {
+  const handleClick = (e) => {
+    if (Notification.permission === 'denied') {
+      // eslint-disable-next-line no-alert
+      alert('Please allow notifications first');
+      return;
+    }
+
+    const isSubscribed = (e.target.dataset.checked === 'true');
+
+    if (isSubscribed) {
+      unsubscribePush();
+    } else {
+      subscribePush();
+    }
+  };
+
   return (
     <header>
       <ContentWrap>
         <Link to="/">
           <h1>b-reminded</h1>
         </Link>
+        <button type="button" aria-label="enable/disable push notifications" onClick={handleClick} />
       </ContentWrap>
     </header>
   );
